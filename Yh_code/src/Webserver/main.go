2d7c8f8b974caf8774a69c90c9b832c43bc7610a
cserli <1528884535@qq.com>
2018-09-27 14:04:52 +0800

    Initial commit

---
+++ Yh_code/src/Webserver/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+
+	_ "golang.org/x/net/websocket" // websocket  -- 数据传输操作
+)
+
+func init() {
+
+	fmt.Println("航语 WebServer")
+	return
+}
+
+func main() {
+
+	return
+}
