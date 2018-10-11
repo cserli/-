e3c6fe86341d400c2570078ba3fa053ab16cd7a9
cserli <1528884535@qq.com>
2018-10-11 18:32:41 +0800

    data  update

--- Yh_code/src/consul/consul_test.go
+++ Yh_code/src/consul/consul_test.go
@@ -11,18 +11,19 @@ import (
 
 const Id = "1234567890"
 
+// 注册新服务
 func TestRegister(t *testing.T) {
 
 	fmt.Println("test begin .")
 	config := consulapi.DefaultConfig()
-	//config.Address = "localhost"
+	// config.Address = "localhost"
 	fmt.Println("defautl config : ", config)
 	client, err := consulapi.NewClient(config)
 	if err != nil {
 		log.Fatal("consul client error : ", err)
 	}
 
-	//创建一个新服务。
+	// 创建一个新服务。
 	registration := new(consulapi.AgentServiceRegistration)
 	registration.ID = Id + "01"
 	registration.Name = "user-tomcat"
@@ -30,14 +31,14 @@ func TestRegister(t *testing.T) {
 	registration.Tags = []string{"group:user-tomcat-01"}
 	registration.Address = "127.0.0.1"
 
-	//增加check。
+	// 增加check。
 	check := new(consulapi.AgentServiceCheck)
 	check.HTTP = fmt.Sprintf("http://%s:%d%s", registration.Address, registration.Port, "/check")
-	//设置超时 5s。
+	// 设置超时 5s。
 	check.Timeout = "5s"
-	//设置间隔 5s。
+	// 设置间隔 5s。
 	check.Interval = "5s"
-	//注册check服务。
+	// 注册check服务。
 	registration.Check = check
 	log.Println("get registration : ", registration)
 	err = client.Agent().ServiceRegister(registration)
@@ -45,7 +46,7 @@ func TestRegister(t *testing.T) {
 		log.Fatal("register server error : ", err)
 	}
 
-	//设置第二个对象。注册两个tomcat。
+	// 设置第二个对象。注册两个tomcat。
 	registration.ID = Id + "02"
 	registration.Name = "user-tomcat"
 	registration.Address = "10.0.2.15"
@@ -56,7 +57,7 @@ func TestRegister(t *testing.T) {
 		log.Fatal("register server error : ", err)
 	}
 
-	//设置第3个对象。注册两个tomcat。
+	// 设置第3个对象。注册两个tomcat。
 	registration.ID = Id + "03"
 	registration.Name = "user-tomcat"
 	registration.Address = "10.0.2.16"
@@ -74,7 +75,7 @@ func TestDregister(t *testing.T) {
 
 	fmt.Println("test begin .")
 	config := consulapi.DefaultConfig()
-	//config.Address = "localhost"
+	// config.Address = "localhost"
 	fmt.Println("defautl config : ", config)
 	client, err := consulapi.NewClient(config)
 	if err != nil {
@@ -85,7 +86,6 @@ func TestDregister(t *testing.T) {
 	if err != nil {
 		log.Fatal("register server error : ", err)
 	}
-
 }
 
 func TestLookup(t *testing.T) {
